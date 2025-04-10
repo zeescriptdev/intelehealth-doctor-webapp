6758f6eabec61e39a4e794255d8837f32f69989a
Zeeshan <zeeshan@intelehealth.org>
2025-04-10 15:46:56 +0530

    NAS-885: chore: Remove aiddx-library and update configuration files
    - Deleted the aiddx-library project and its associated files, including package.json, README.md, and service files.
    - Updated angular.json to remove references to the deleted library.
    - Added .npmrc file to set legacy-peer-deps to true for compatibility.
    - Updated package.json and package-lock.json to include aiddx-library dependency from GitHub.
    - Cleaned up tsconfig.json by removing paths related to aiddx-library.

--- projects/aiddx-library/src/public-api.ts
+++
@@ -1,8 +0,0 @@
-/*
- * Public API Surface of aiddx-library
- */
-
-export * from './services/aiddx.service';
-export * from './lib/aillmddx/aillmddx.component';
-export * from './lib/aiddx-library.module';
-export * from './lib/token';
