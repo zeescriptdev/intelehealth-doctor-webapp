d0f52888004e158920d7bfac30493dec04174f63
AkshayIntelehealth <akshay@intelehealth.org>
2025-04-23 16:16:56 +0530

    fix: popup for data lost

---
+++ src/app/core/guards/visit-summary-deactivate.guard.ts
@@ -0,0 +1,20 @@
+import { Injectable } from "@angular/core";
+import { ActivatedRouteSnapshot, CanDeactivate, RouterStateSnapshot } from "@angular/router";
+import { VisitSummaryComponent } from "src/app/dashboard/visit-summary/visit-summary.component";
+
+@Injectable()
+export class CanDeactivateVisitSummary implements CanDeactivate<VisitSummaryComponent> {
+    canDeactivate(component: VisitSummaryComponent, currentRoute: ActivatedRouteSnapshot,
+        currentState: RouterStateSnapshot,
+        nextState: RouterStateSnapshot): boolean {
+        if(component.changesMade){
+            component.onExitPageConfirmDialog(nextState.url,'You have unsaved changes that will be lost.');
+            return false;
+        } else if(component.isCallInProgress){
+            component.onExitPageConfirmDialog(nextState.url,'Whatsapp Call In-progress');
+            return false;
+        }else {
+            return true;
+        }
+    }
+}
