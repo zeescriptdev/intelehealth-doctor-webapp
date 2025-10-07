76506147d525427450d866bbf71df34727cf524c
dhanashri.p <dhanashri@intelehealth.org>
2025-10-07 13:18:14 +0530

    AEAT-1912 Added NCD reports for admin

--- src/app/core/interceptors/loader.interceptor.ts
+++ src/app/core/interceptors/loader.interceptor.ts
@@ -24,7 +24,9 @@ export class LoaderInterceptor implements HttpInterceptor {
         '/bs',
         '/vlrv',
         '/lcrep',
-        '/attribute'
+        '/attribute',
+        '/ncd',
+        '/ncdsummary'
     ];
 
     const shouldSkip = excludedPatterns.some(pattern =>
